62e2d2a074802429fc6aaf7e69335b7ce106c256
Patrick Raco <praco@telecomsys.com>
2017-10-02 22:57:35 +0000

    chef: convert hostname playbook to amzn linux

--- exercises/auto_webserver/chef/cookbooks/hostname/CHANGELOG.md
+++ exercises/auto_webserver/chef/cookbooks/hostname/CHANGELOG.md
@@ -5,3 +5,6 @@ This file is used to list changes made in each version of the hostname cookbook.
 v0.0.1 (01-OCT-2017)
 --------------------
 - [PAR] - Initial version
+v0.0.2 (02-OCT-2017)
+--------------------
+- [PAR] - Changed to Amazon Linux
--- exercises/auto_webserver/chef/cookbooks/hostname/recipes/default.rb
+++ exercises/auto_webserver/chef/cookbooks/hostname/recipes/default.rb
@@ -5,55 +5,20 @@
 
 #node.name = Chef::Config[:node_name]
 
-# Update the server's repos apt package DB (sudo apt-get update)
-execute "apt-get-update-periodic" do
-  command "apt-get update"
-  ignore_failure true
-  only_if do
-    File.exists?('/var/lib/apt/periodic/update-success-stamp') &&
-    File.mtime('/var/lib/apt/periodic/update-success-stamp') < Time.now - 86400
-  end
-end
-
-service "hostname" do
-  supports :restart => true
-end
-
-service "networking" do
-  supports :restart => true
-end
-
 template "/etc/hosts" do
   source "hosts.erb"
   owner 'root'
   group 'root'
   mode 00644
-  # for some reason this didn't work, but it's been a while
-  # so i don't remember why.
-  # leaving in so i can figure it out and fix someday
-  #notifies :restart, "service[hostname]"
-  #notifies :restart, "service[networking]"
 end
 
-file '/etc/hostname' do
-  content "#{node.name}"
+template '/etc/sysconfig/network' do
+  source "etc_sysconfig_network.erb"
   owner 'root'
   group 'root'
   mode 00644
-  # for some reason this didn't work, but it's been a while
-  # so i don't remember why.
-  # leaving in so i can figure it out and fix someday
-  #notifies :restart, "service[hostname]"
-  #notifies :restart, "service[networking]"
 end
 
-execute "restart hostname service" do
-  command "sudo service hostname restart"
+execute "restart rsyslog service" do
+  command "sudo service rsyslog restart"
 end
-
-# don't think this is needed, but it's been a while
-# so i don't remember why.
-# leaving in if I ever get the desire to fix
-#execute "restart networking service" do
-#  command "sudo service networking restart"
-#end
